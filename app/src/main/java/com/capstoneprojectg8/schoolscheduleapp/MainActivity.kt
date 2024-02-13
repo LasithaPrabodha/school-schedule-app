241c255231930446882f9064987d55111c1a78f3
poncegamez1 <poncegamez1@gmail.com>
2024-02-13 12:15:15 -0500

    [#1] back arrow in classes fragment now working.

--- app/src/main/java/com/capstoneprojectg8/schoolscheduleapp/MainActivity.kt
+++ app/src/main/java/com/capstoneprojectg8/schoolscheduleapp/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.capstoneprojectg8.schoolscheduleapp.database.ClassesDatabase
@@ -18,6 +19,7 @@ import com.capstoneprojectg8.schoolscheduleapp.ui.settings.classes.ClassesViewMo
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     lateinit var classesViewModel: ClassesViewModel
 
@@ -32,7 +34,7 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
-        val appBarConfiguration = AppBarConfiguration(
+        appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_settings
             )
@@ -41,6 +43,11 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     private fun setUpViewModel(){
